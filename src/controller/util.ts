47e07fefa51a808697ecadd0ba801ccf457a0de3
AxelSheva <axelsheva94@gmail.com>
2019-10-19 17:34:05 +0300

    Add request for making key pair from secret

--- src/controller/util.ts
+++ src/controller/util.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { generatePassphrase } from 'ddk.registry/dist/util/passphrase';
 import { ResponseEntity } from 'ddk.registry/dist/model/common/responseEntity';
+import { createKeyPairBySecret } from 'ddk.registry/dist/util/crypto';
 
 export class UtilController {
     generatePassphrase(_req: Request, res: Response): Response {
@@ -8,6 +9,26 @@ export class UtilController {
         const response = new ResponseEntity({ data: passphrase });
 
         return res.send(response);
+    }
+
+    makeKeyPair(req: Request, res: Response): Response {
+        const { secret } = req.body;
+        if (!secret) {
+            return res.send({
+                success: false,
+                errors: ['Secret parameter is missing in the query string'],
+            });
+        }
+
+        const keyPair = createKeyPairBySecret(secret);
+
+        return res.send({
+            success: true,
+            data: {
+                publicKey: keyPair.publicKey.toString('hex'),
+                privateKey: keyPair.privateKey.toString('hex'),
+            },
+        });
     }
 }
 
