3754717105e3d5478aeef8dcc4a6647f72edae9f
axelsheva <axelsheva94@gmail.com>
2019-11-04 14:10:20 +0200

    Add getting version from package.json on build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 REPOSITORY_NAME="ddkoin/rest-api-client"
-TAG="stable"
+TAG=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
 
 docker build -f build.Dockerfile -t $REPOSITORY_NAME:$TAG .
