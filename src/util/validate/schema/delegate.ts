3edc9b23d0c217266875bc602bb7f3af086db93d
Usman <usman_ahmad888@yahoo.com>
2020-07-14 12:56:16 +0500

    resolved linting errors

--- src/util/validate/schema/delegate.ts
+++ src/util/validate/schema/delegate.ts
@@ -1,37 +1,36 @@
 import { PAGINATION_SCHEME } from 'ddk.registry/dist/util/validate/scheme/filter';
 
-export const getDelegates =
-    {
-        id: 'POST /api/delegate/getDelegates',
-        type: 'object',
-        properties: {
-            ...PAGINATION_SCHEME,
-            username: {
-                type: 'string',
-                minLength: 3,
-            },
-            sort: {
+export const getDelegates = {
+    id: 'POST /api/delegate/getDelegates',
+    type: 'object',
+    properties: {
+        ...PAGINATION_SCHEME,
+        username: {
+            type: 'string',
+            minLength: 3,
+        },
+        sort: {
+            type: 'array',
+            items: {
                 type: 'array',
                 items: {
-                    type: 'array',
-                    items: {
-                        type: 'string',
-                        enum: ['ASC', 'DESC', 'approval', 'publicKey', 'votes', 'username'],
-                    },
+                    type: 'string',
+                    enum: ['ASC', 'DESC', 'approval', 'publicKey', 'votes', 'username'],
                 },
             },
         },
-        required: ['limit', 'offset'],
+    },
+    required: ['limit', 'offset'],
 };
 
-export const getActiveDelegates={
+export const getActiveDelegates = {
 
     id: 'POST /api/delegate/getActiveDelegates',
     type: 'object',
     properties: {
         ...PAGINATION_SCHEME,
     },
-    required: ['limit', 'offset']
+    required: ['limit', 'offset'],
 };
 
 
